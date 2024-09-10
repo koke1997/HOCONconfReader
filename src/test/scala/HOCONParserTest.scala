e3f673a196e39bce776b1d4d4b78ea833bfe3f16
koke1997 <67540157+koke1997@users.noreply.github.com>
2024-09-10 17:34:14 +0200

    Fix test file to use parseConfig method
    Update `HOCONParserTest.scala` to fix test issues and add a new test case.
    
    * Import `java.io.File` to use `ConfigFactory.parseFile`.
    * Replace `HOCONParser.getAllKeys` with `HOCONParser.parseConfig`.
    * Use `ConfigFactory.parseFile(new File("src/test/resources/test.conf"))` instead of `ConfigFactory.parseResources("test.conf")`.
    * Add a new test case to verify the `parseConfig` method.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/koke1997/HOCONconfReader?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/test/scala/HOCONParserTest.scala
+++ src/test/scala/HOCONParserTest.scala
@@ -2,6 +2,7 @@ package com.example
 
 import org.scalatest.funsuite.AnyFunSuite
 import com.typesafe.config.ConfigFactory
+import java.io.File
 
 class HOCONParserTest extends AnyFunSuite {
 
@@ -20,7 +21,7 @@ class HOCONParserTest extends AnyFunSuite {
         |""".stripMargin
 
     val config = ConfigFactory.parseString(sampleConfig)
-    val uniqueKeys = HOCONParser.getAllKeys(config)
+    val uniqueKeys = HOCONParser.parseConfig("src/test/resources/test.conf")
 
     val expectedKeys = Set(
       "play.application.name",
@@ -32,14 +33,40 @@ class HOCONParserTest extends AnyFunSuite {
   }
 
   test("HOCONParser should correctly parse included configuration files") {
-    val config = ConfigFactory.parseResources("test.conf")
-    val uniqueKeys = HOCONParser.getAllKeys(config)
+    val config = ConfigFactory.parseFile(new File("src/test/resources/test.conf"))
+    val uniqueKeys = HOCONParser.parseConfig("src/test/resources/test.conf")
 
     val expectedKeys = Set(
       "included.settingA",
       "included.settingB",
       "main.setting1",
       "main.setting2"
+    )
+
+    assert(uniqueKeys == expectedKeys)
+  }
+
+  test("HOCONParser should correctly parse a sample Play Framework .conf file") {
+    val sampleConfig =
+      """
+        |play {
+        |  application {
+        |    name = "example"
+        |    version = "1.0"
+        |  }
+        |  http {
+        |    port = 9000
+        |  }
+        |}
+        |""".stripMargin
+
+    val config = ConfigFactory.parseString(sampleConfig)
+    val uniqueKeys = HOCONParser.parseConfig("src/test/resources/test.conf")
+
+    val expectedKeys = Set(
+      "play.application.name",
+      "play.application.version",
+      "play.http.port"
     )
 
     assert(uniqueKeys == expectedKeys)
