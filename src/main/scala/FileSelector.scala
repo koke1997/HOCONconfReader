d992b4239e63b8fbc12a9a42dc19d7229145de04
koke1997 <67540157+koke1997@users.noreply.github.com>
2024-09-11 12:02:45 +0200

    Added sort functionality
    Sorting by keys from A to Z

--- src/main/scala/FileSelector.scala
+++ src/main/scala/FileSelector.scala
@@ -6,7 +6,7 @@ object FileSelector {
   def main(args: Array[String]): Unit = {
     println("Please enter the path to the HOCON file you want to read:")
     val filePath = readLine()
-    val uniqueKeys = HOCONParser.parseConfig(filePath)
+    val uniqueKeys = HOCONParser.parseConfig(filePath).toList.sorted
     uniqueKeys.foreach { key =>
       val value = HOCONParser.getConfigValue(filePath, key)
       println(s"$key: $value")
